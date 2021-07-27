75cf4e6cf1aa951a5430108d4c8bfd3d92ae9d71
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-07-27 15:21:48 +0200

    Flytter fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import AutomatiskBehandling from './AutomatiskBehandling'
 import Vis from './components/vis'
@@ -18,40 +18,12 @@ import Sykedager from './Sykedager'
 import Sykmeldinger from './Sykmeldinger'
 import Utbetalingsdager from './Utbetalingsdager'
 import Utbetalingstype from './Utbetalingstype'
-import env from './utils/environment'
 
 
 function App() {
 
-    const { fikk401, fodselsnummer, setFodselsnummer, setFikk401 } = useAppStore()
+    const { fikk401, fodselsnummer } = useAppStore()
 
-    useEffect(() => {
-        async function fetchData() {
-            const data = await fetch(`${env.flexInternGatewayRoot}/spinnsyn-backend-testdata/api/v1/testdata/fnr`, {
-                method: 'GET',
-                credentials: 'include',
-                headers: { 'Content-Type': 'application/json' }
-            })
-            if (data.ok) {
-                const fnr = await data.json()
-                setFodselsnummer(fnr)
-            } else {
-                if (data.status === 401) {
-                    setFikk401(true)
-                } else {
-                    window.alert('Oops, noe gikk galt ved henting av fnr')
-                }
-            }
-        }
-
-        fetchData().catch((e: any) => {
-            // eslint-disable-next-line no-console
-            console.error('OIDA', e)
-            window.alert(`Ooops, feil. ved fnr fetching! ${e}`)
-        }
-        )
-
-    }, [ setFodselsnummer, setFikk401 ])
 
     return (
         <div style={{
@@ -61,11 +33,11 @@ function App() {
             fontFamily: '"Courier New", Courier, monospace'
         }}>
             <h1 style={{ textAlign: 'center' }}>Vedtak testdatagenerator</h1>
+            <Fodselsnummer />
             <Vis hvis={fikk401}>
                 <FeilmeldingInnlogging />
             </Vis>
             <Vis hvis={fodselsnummer !== ''}>
-                <Fodselsnummer />
                 <Sykmeldinger />
                 <Soknader />
                 <Datoer />
--- src/Fodselsnummer.tsx
+++ src/Fodselsnummer.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { useAppStore } from './stores/app-store'
+import env from './utils/environment'
 
 function Fodselsnummer() {
-    const { fodselsnummer } = useAppStore()
+    const { fodselsnummer, setFodselsnummer, setFikk401 } = useAppStore()
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetch(`${env.flexInternGatewayRoot}/spinnsyn-backend-testdata/api/v1/testdata/fnr`, {
+                method: 'GET',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' }
+            })
+            if (data.ok) {
+                const fnr = await data.json()
+                setFodselsnummer(fnr)
+            } else {
+                if (data.status === 401) {
+                    setFikk401(true)
+                } else {
+                    window.alert('Oops, noe gikk galt ved henting av fnr')
+                }
+            }
+        }
+
+        fetchData().catch((e: any) => {
+            // eslint-disable-next-line no-console
+            console.error('OIDA', e)
+            window.alert(`Ooops, feil. ved fnr fetching! ${e}`)
+        }
+        )
+
+    }, [ setFodselsnummer, setFikk401 ])
 
     return (
         <div style={{ border: '1px solid', padding: '1em' }}>
